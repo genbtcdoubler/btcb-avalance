c9e7f051df00e9e1dd7949a080c45ac9729188a3
genbtcdoubler <79389495+genbtcdoubler@users.noreply.github.com>
2025-09-14 18:33:52 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,13 @@
-
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
 
   return {
     plugins: [react()],
     define: {
-      __MINING_INTERVAL__: JSON.stringify(env.MINING_INTERVAL || '9999999999999999999999999999'),
-      __API_URL__: JSON.stringify(env.API_URL || 'https://default-api.com')
+      __MINING_INTERVAL__: JSON.stringify(env.MINING_INTERVAL || '1000')
     },
     build: {
       outDir: 'dist',
